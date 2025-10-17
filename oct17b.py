6b6cb9a90c1ae831dbcbea2ee1c0ebde2f2e6c61
Sinsina V <sinsinav505@gmail.com>
2025-10-17 12:02:24 +0530

    Histogram Customer Age Distribution

--- oct17b.py
+++ oct17b.py
@@ -1,7 +1,26 @@
-# 📈 HISTOGRAM QUESTIONS
+#  HISTOGRAM QUESTIONS
 # ============================================================
 
 # Sample Dataset ⿣: Customer Age Distribution
+'''df_age = pd.DataFrame({
+    'Customer Age': [18, 20, 22, 22, 25, 26, 27, 29, 30, 32, 34, 35, 36, 38, 40, 42, 45, 48, 50, 52]
+})'''
+
+# ⿣ Plot a histogram of the 'Customer Age' column to show the distribution of ages.
+#     - Set the number of bins to 10.
+#     - Add proper labels and a title.
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
 df_age = pd.DataFrame({
     'Customer Age': [18, 20, 22, 22, 25, 26, 27, 29, 30, 32, 34, 35, 36, 38, 40, 42, 45, 48, 50, 52]
 })
+
+print(df_age)
+
+plt.hist(df_age['Customer Age'],bins=10)
+plt.title("Customer Age Distribution")
+plt.xlabel('Age')
+
+plt.show()
